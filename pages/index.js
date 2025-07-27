bace12f4c997d0df99abe7cc027ba002b5e99591
ryzx <rezzzyxz@gmail.com>
2025-07-27 21:58:55 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: 40, textAlign: 'center' }}>
+      <h1>🧠 Ryezx API Playground</h1>
+      <p>Try: <code>/downloader/tiktok?url=...</code></p>
+    </main>
+  );
+}
